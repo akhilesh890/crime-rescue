2cb0fd52e69d9c4b7d3d22ee0b7fd44e42b00b53
Akhilesh <akhilesh890@gmail.com>
2014-04-25 00:00:25 -0500

    Initial Commit

---
+++ CrimeRescue/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CrimeRescue
+//
+//  Created by Aswin Akhilesh on 4/25/14.
+//  Copyright (c) 2014 Aswin Akhilesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CrimeRescue/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CrimeRescue
+//
+//  Created by Aswin Akhilesh on 4/25/14.
+//  Copyright (c) 2014 Aswin Akhilesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
